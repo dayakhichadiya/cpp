123b94c87493ef6d2bbfe095525613bd699e38c6
harsh <harshchavada174@gmail.com>
2024-04-26 17:51:34 +0530

    update

--- obj_array.cpp
+++ obj_array.cpp
@@ -7,7 +7,7 @@ int id;
 char name[50];
 int marks[5];
 float percentage;
-float per(float total);
+float result(float total);
 
 public:
 
@@ -25,25 +25,23 @@ void  getdata() {
 void showdata() {
     cout << "Student Id is : " <<id<<endl;
     cout << "Name of the student is : " <<name<<endl;
-    cout<<"the percentage is :"<<per(percentage)<<endl;
+    cout<<"the percentage is :"<<result(percentage)<<endl;
  }
 };
-float Student :: per(float total){
+float Student :: result(float total){
     total=0;
     for(int i=0;i<5;i++){
         total+=marks[i];
     }
-    float  percentage=total*100/500;
+    float percentage=total*100/500;
     return percentage;
 
 }
 int main(){
     Student s1[3];
-    for(int i=0;i<3;i++){
-        cout<<"enter the student :"<<i+1<<endl;
+    for(int i=1;i<=3;i++){
+        cout<<"enter the student :"<<i<<endl;
         s1[i].getdata();
         s1[i].showdata();
-
-
     }
 }
